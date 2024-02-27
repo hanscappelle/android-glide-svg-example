b93f3074dde5ae86c47dc2e916aa1a1d05c68260
Hans Cappelle <hans@roov.app>
2024-02-27 19:04:46 +0100

    initial project commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+}
+
+android {
+    namespace = "be.hcpl.android.glidesvgrecyclerissue"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "be.hcpl.android.glidesvgrecyclerissue"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation("com.caverock:androidsvg:1.2.1")
+    //annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
+
+}
---
+++ app/src/main/java/be/hcpl/android/glidesvgrecyclerissue/MainActivity.kt
@@ -0,0 +1,69 @@
+package be.hcpl.android.glidesvgrecyclerissue
+
+import android.app.Activity
+import android.graphics.drawable.PictureDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+
+
+class MainActivity : Activity() {
+
+    private val svgPath = "https://www.clker.com/cliparts/u/Z/2/b/a/6/android-toy-h.svg"
+
+    private val pngPath =
+        "https://upload.wikimedia.org/wikipedia/commons/d/de/Windows_live_square.JPG"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // add support for SVG
+        // example from https://github.com/bumptech/glide/blob/v4.10.0/samples/svg/
+        //val requestBuilder: RequestBuilder<PictureDrawable> = GlideApp.with(baseContext)
+        //    .`as`(PictureDrawable::class.java)
+        //    .listener(SvgSoftwareLayerSetter())
+
+        // single image, no issue
+        val imageView: ImageView = findViewById(R.id.single_image)
+        Glide.with(baseContext).load(svgPath).into(imageView)
+        //requestBuilder.load(svgPath).centerCrop().into(imageView)
+
+        // same image used in recyclerView = ISSUE with centerCrop, limited to SVG
+        val recyclerView: RecyclerView = findViewById(R.id.recycler)
+        val adapter = GlideAdapter(listOf(svgPath, pngPath, svgPath))
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+}
+
+class GlideAdapter(
+    private val items: List<String>,
+) : RecyclerView.Adapter<GlideViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = GlideViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: GlideViewHolder, position: Int) {
+        // use Glide here for image loading
+        Glide.with(holder.itemView.context).load(items[position]).into(holder.imageView)
+        //requestBuilder.load(items[position]).into(holder.imageView)
+    }
+
+    override fun getItemCount() = items.size
+
+}
+
+class GlideViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val imageView: ImageView = itemView.findViewById(R.id.recycler_image)
+
+}
